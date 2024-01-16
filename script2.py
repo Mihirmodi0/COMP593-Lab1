7bdb0b1f23bb0b112d44c61162fb5d90c95a2f2d
Mihirmodi0 <156608270+Mihirmodi0@users.noreply.github.com>
2024-01-16 11:19:01 -0500

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
  # There should be one and preferably only one obvious way to do it.
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("In the face of ambiguity, refuse the temptation to guess.")
+print("Now is better than never..")
